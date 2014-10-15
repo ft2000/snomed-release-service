b9d2c87b3f7a9e84aa05d60817107ceff237afab
CoderMChu <mch@ihtsdo.org>
2014-10-15 11:42:45 +0100

    Transforming SimpleRefsetMap files for legacy ids

--- api/src/test/java/org/ihtsdo/buildcloud/integration/corecomponents/CoreComponentsTestIntegration.java
+++ api/src/test/java/org/ihtsdo/buildcloud/integration/corecomponents/CoreComponentsTestIntegration.java
@@ -1,12 +1,12 @@
 package org.ihtsdo.buildcloud.integration.corecomponents;
 
+import java.io.IOException;
+import java.util.zip.ZipFile;
+
 import org.ihtsdo.buildcloud.controller.AbstractControllerTest;
 import org.ihtsdo.buildcloud.controller.helper.IntegrationTestHelper;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.util.zip.ZipFile;
 
 public class CoreComponentsTestIntegration extends AbstractControllerTest {
 
@@ -49,30 +49,31 @@ public class CoreComponentsTestIntegration extends AbstractControllerTest {
 
 	}
 
-	private void executeAndVerfiyResults(String releaseDate) throws Exception, IOException {
-		String executionURL1 = integrationTestHelper.createExecution();
+	private void executeAndVerfiyResults(final String releaseDate) throws Exception, IOException {
+		final String executionURL1 = integrationTestHelper.createExecution();
 		integrationTestHelper.triggerExecution(executionURL1);
 		integrationTestHelper.publishOutput(executionURL1);
 
 		// Assert first release output expectations
-		String expectedZipFilename = "SnomedCT_Release_INT_"+releaseDate+".zip";
-		String expectedZipEntries = createExpectedZipEntries(releaseDate);
-		ZipFile zipFile = integrationTestHelper.testZipNameAndEntryNames(executionURL1, expectedZipFilename, expectedZipEntries, getClass());
+		final String expectedZipFilename = "SnomedCT_Release_INT_"+releaseDate+".zip";
+		final String expectedZipEntries = createExpectedZipEntries(releaseDate);
+		final ZipFile zipFile = integrationTestHelper.testZipNameAndEntryNames(executionURL1, expectedZipFilename, expectedZipEntries, getClass());
 
 		integrationTestHelper.assertZipContents("expectedoutput", zipFile, getClass());
 	}
 
-	private void loadDeltaFilesToInputDirectory(String releaseDate) throws Exception {
+	private void loadDeltaFilesToInputDirectory(final String releaseDate) throws Exception {
 		integrationTestHelper.uploadManifest("core_manifest_"+releaseDate+".xml", getClass());
 		integrationTestHelper.uploadDeltaInputFile("rel2_Concept_Delta_INT_" + releaseDate + ".txt", getClass());
 		integrationTestHelper.uploadDeltaInputFile("rel2_Description_Delta-en_INT_"+releaseDate +".txt", getClass());
 		integrationTestHelper.uploadDeltaInputFile("rel2_TextDefinition_Delta-en_INT_"+releaseDate +".txt", getClass());
 		integrationTestHelper.uploadDeltaInputFile("rel2_StatedRelationship_Delta_INT_"+releaseDate +".txt", getClass());
 		integrationTestHelper.uploadDeltaInputFile("rel2_cRefset_LanguageDelta-en_INT_" + releaseDate +".txt", getClass());
+		integrationTestHelper.uploadDeltaInputFile("rel2_sRefset_SimpleMapDelta_INT_" + releaseDate +".txt", getClass());
 	}
 
-	private String createExpectedZipEntries(String effectiveTime) {
-		String expectedZipEntries =
+	private String createExpectedZipEntries(final String effectiveTime) {
+		final String expectedZipEntries =
 			INTERNATIONAL_RELEASE + effectiveTime + "/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/Readme_" + effectiveTime + ".txt\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/\n" +
@@ -80,6 +81,8 @@ public class CoreComponentsTestIntegration extends AbstractControllerTest {
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Full/Refset/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Full/Refset/Language/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Full/Refset/Language/der2_cRefset_LanguageFull-en_INT_" + effectiveTime + ".txt\n" +
+			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Full/Refset/Map/\n" +
+			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Full/Refset/Map/der2_sRefset_SimpleMapFull_INT_" + effectiveTime + ".txt\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Full/Terminology/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Full/Terminology/sct2_Concept_Full_INT_"+effectiveTime+".txt\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Full/Terminology/sct2_Description_Full-en_INT_"+effectiveTime+".txt\n" +
@@ -89,6 +92,8 @@ public class CoreComponentsTestIntegration extends AbstractControllerTest {
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Snapshot/Refset/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Snapshot/Refset/Language/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Snapshot/Refset/Language/der2_cRefset_LanguageSnapshot-en_INT_"+ effectiveTime +".txt\n" +
+			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Snapshot/Refset/Map/\n" +
+			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Snapshot/Refset/Map/der2_sRefset_SimpleMapSnapshot_INT_" + effectiveTime + ".txt\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Snapshot/Terminology/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Snapshot/Terminology/sct2_Concept_Snapshot_INT_"+effectiveTime+".txt\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Snapshot/Terminology/sct2_Description_Snapshot-en_INT_"+effectiveTime+".txt\n" +
@@ -98,6 +103,8 @@ public class CoreComponentsTestIntegration extends AbstractControllerTest {
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Delta/Refset/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Delta/Refset/Language/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Delta/Refset/Language/der2_cRefset_LanguageDelta-en_INT_"+ effectiveTime + ".txt\n" +
+			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Delta/Refset/Map/\n" +
+			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Delta/Refset/Map/der2_sRefset_SimpleMapDelta_INT_" + effectiveTime + ".txt\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Delta/Terminology/\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Delta/Terminology/sct2_Concept_Delta_INT_"+ effectiveTime +".txt\n" +
 			INTERNATIONAL_RELEASE + effectiveTime + "/RF2Release/Delta/Terminology/sct2_Description_Delta-en_INT_"+ effectiveTime +".txt\n" +
